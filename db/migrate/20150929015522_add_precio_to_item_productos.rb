b57ceedc570bcbd1b8390c5bd2c5c89b02a557bb
diegomotta <diegomotta18@gmail.com>
2015-10-03 22:03:55 -0300

    relase

--- db/migrate/20150929015522_add_precio_to_item_productos.rb
+++
@@ -1,5 +0,0 @@
-class AddPrecioToItemProductos < ActiveRecord::Migration
-  def change
-  	add_column :item_productos, :precio, :string
-  end
-end
--- db/migrate/20151003170757_add_descripcion_to_item_de_infos.rb
+++
@@ -1,7 +0,0 @@
-class AddDescripcionToItemDeInfos < ActiveRecord::Migration
-  def change
-  	remove_column :item_de_infos, :descrición, :string
-  	add_column :item_de_infos, :descripción, :text
-
-  end
-end
