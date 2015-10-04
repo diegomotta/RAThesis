f7e6f3d39cd9b854240349db987e0b8755739fc1
diegomotta <diegomotta18@gmail.com>
2015-10-03 22:26:54 -0300

    release descripcion

--- db/migrate/20151004011226_remove_descripcion_from_item_de_infos.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDescripcionFromItemDeInfos < ActiveRecord::Migration
-  def change
-  	remove_column :item_de_infos, :descripción
-  end
-end
---
+++ db/migrate/20151004012403_add_descripcion_from_item_de_infos.rb
@@ -0,0 +1,5 @@
+class AddDescripcionFromItemDeInfos < ActiveRecord::Migration
+  def change
+  	add_column :item_de_infos, :descripción, :text
+  end
+end
