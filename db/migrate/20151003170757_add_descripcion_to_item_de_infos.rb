df50628b2005c42dbd7910421c9e31a372876e86
diegomotta <diegomotta18@gmail.com>
2015-10-03 22:00:16 -0300

    si

--- db/migrate/20151003170757_add_descripcion_to_item_de_infos.rb
+++ db/migrate/20151003170757_add_descripcion_to_item_de_infos.rb
@@ -1,5 +1,7 @@
 class AddDescripcionToItemDeInfos < ActiveRecord::Migration
   def change
+  	remove_column :item_de_infos, :descrición, :string
   	add_column :item_de_infos, :descripción, :text
+
   end
 end
